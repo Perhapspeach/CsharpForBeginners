3300924f0fa6e7dc8402cd64faf95c8f3972ba0e
Perhapspeach <79052106+Perhapspeach@users.noreply.github.com>
2021-03-30 19:13:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите возраст:   ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age > 17) 
+            {
+                Console.WriteLine("Введите имя:       ");
+            }
+             else
+            {
+                Console.WriteLine("Брысь !!!");
+            }
+        }
+    
+    }
+}
